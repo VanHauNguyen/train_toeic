e4d19b06f78dd590543514b0d9600a6785a8aca4
台鋼科大_阮文后 <7q13124127@mail.tsust.edu.tw>
2026-05-25 09:59:08 +0800

    content quality upgrade

--- frontend/.next/server/middleware-build-manifest.js
+++ frontend/.next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-a7426602a000e6dd.js","static/chunks/4bd1b696-409494caf8c83275.js","static/chunks/255-e881f48ae1d2333a.js","static/chunks/main-app-8adbc3ce90449684.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-a7426602a000e6dd.js","static/chunks/framework-3457b9c2619cdd96.js","static/chunks/main-8744520a8a31e6ae.js","static/chunks/pages/_app-5addca2b3b969fde.js"],"/_error":["static/chunks/webpack-a7426602a000e6dd.js","static/chunks/framework-3457b9c2619cdd96.js","static/chunks/main-8744520a8a31e6ae.js","static/chunks/pages/_error-022e4ac7bbb9914f.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
